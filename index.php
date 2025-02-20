86c22216232568f9a26ca7c77f6716651f66437a
amirah <mirez.data@gmail.com>
2025-02-20 08:48:45 +0800

    add learning resources

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Demo</title>
+
+</head>
+<body>
+  <h1>
+    <?php
+      echo "Hello World!"; 
+      include 'variable.php'; 
+      include 'conditional.php';
+    ?>
+  </h1>
+</body>
+
+</html>
+
---
+++ variable.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Learning Variable</title>
+
+</head>
+<body>
+  <h1>
+    <?php
+      $name = "Sylus";
+      echo "Welcome home! " . $name; 
+    ?>
+  </h1>
+</body>
+
+</html>
